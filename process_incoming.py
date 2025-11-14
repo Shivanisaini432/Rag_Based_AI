3e46f8332776a8dcd8db2fa8ad03bea5c4344b0b
Shivani Saini <sainishivani1333@gmail.com>
2025-11-15 03:51:32 +0530

    Processes Incoming

---
+++ process_incoming.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+import requests
+import numpy as np 
+import joblib
+
+
+def create_embedding(text_list):
+    r = requests.post("http://localhost:11434/api/embed", json={
+        "model": "bge-m3",
+        "input": text_list
+    })
+
+    embedding = r.json()['embeddings']
+    return embedding
+
+
+def inference(prompt):
+    r = requests.post("http://localhost:11434/api/generate", json={
+        # "model": "deepseek-r1",
+        "model": "llama3.2",
+        "prompt": prompt,
+        "stream": False
+    })
+
+    response = r.json()
+    print(response)
+    return response
+
+
+
+df =joblib.load('embeddings.joblib')
+
+
+incoming_query =input("Ask a Question: ")
+question_embedding = create_embedding([incoming_query])[0]
+
+# find similarties of question_embedding with other embeddings
+#print(np.vstack(df['embedding'].values))
+#print(np.vstack(df['embedding']).shape)
+
+similarities = cosine_similarity(np.vstack(df['embedding']).values, [question_embedding]).flatten()
+#print(similarities)
+top_result = 5
+max_indx =similarities.argsort()[::-1][0:top_result]
+#print(max_indx)
+new_df = df.loc[max_indx]
+#print(new_df[["title", "number", "text"]])
+
+prompt = f'''I am teaching web devevlopment in my Sigma development course. Here are video subtitle chunks containing video title, video number, start time in seconds, end time in seconds, the text at that time:
+{new_df[{"title", "number", "start", "end", "text"}].to_json(orient="records")}
+--------------------------
+"{incoming_query}"
+user asked this question related to the video chunks, you have to answer in human way (dont mention the above format, its just for you) where and how much content is taught in which video(in which video and at what timestamp) and guide to user to go to that particular video. If user asks unrelated question, tell him that you can only answer question related to the course
+
+'''
+with open("prompt.txt", "w") as f:
+    f.write(prompt)
+    
+response = inference(prompt)["response"]
+print(response)
+
+with open("response.txt", "w") as f:
+    f.write(response)
+# for index, item in new_df.iterrows():
+#     print(index, item["title"], item["number"], item["text"], item["start"], item["end"])
